90606505a32c3bd69809a470d89441d52ca6af80
poxyx <orangutancoder@gmail.com>
2020-04-08 22:24:41 +0800

    fix url problem

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('https://poxyx.github.io/sw.js')
+	navigator.serviceWorker.register(`${window.location.origin}/sw.js`)
 	.then((reg) => console.log('service worker registered',reg))
 	.catch((err) => console.log('service worker not registered',err))
 }
