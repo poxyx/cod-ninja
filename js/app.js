17089d5e0155578d9e245c473762b245f7a8b2f9
poxyx <orangutancoder@gmail.com>
2020-04-08 22:23:15 +0800

    add fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('http://localhost/cod-ninja/sw.js')
+	navigator.serviceWorker.register('https://poxyx.github.io/sw.js')
 	.then((reg) => console.log('service worker registered',reg))
 	.catch((err) => console.log('service worker not registered',err))
 }
